c9bb5d29f3478c02e5acb95daefdf88bc79c0304
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-02-22 12:42:40 -0500

    oop1 done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,53 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Vehicle is the base class
+class Vehicle:
+    # Base class
+    def __init__(self, name):
+        self.name = name
+    pass
+
+# Flight vehicles, descended from Vehicle.
+
+
+class FlightVehicle(Vehicle):
+    # Base of this tree
+    def __init__(self, name):
+        super().__init__(name)
+    pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+    pass
+
+
+class StarShip(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+    pass
+
+# Ground Vehicles, descended from Vehicle
+
+
+class GroundVehicle(Vehicle):
+    # Base of this tree
+    def __init__(self, name):
+        super().__init__(name)
+    pass
+
+
+class Car(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+    pass
